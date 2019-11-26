c88d1b6b40db2aa1d120bd1907fb8add87d216da
Sebastian Rugina <dev@srugina.com>
2019-11-26 11:03:21 +0000

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
     >
       <template v-slot:header>
         <h1 id="home-banner-h1" class="text-center" style="font-weight: 600;">
-          D&amp;Dear Al
+          D&amp;Dear All {{ process.env.VUE_APP_API_URL }}
         </h1>
       </template>
 
