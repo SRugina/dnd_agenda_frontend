05e0a1461d5b622d360084f3538a80becbf998dd
Sebastian Rugina <dev@srugina.com>
2019-11-26 09:48:39 +0000

    purpose miss spell to see if it updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
     >
       <template v-slot:header>
         <h1 id="home-banner-h1" class="text-center" style="font-weight: 600;">
-          D&amp;Dear Al {{ process.env.API_URL }}
+          D&amp;Dear Al
         </h1>
       </template>
 
